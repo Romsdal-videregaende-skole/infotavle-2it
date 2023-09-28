09bc5414e05d66b64433df64a4b66777692748eb
bazic <lashav19@o365.mrvgs.no>
2023-09-28 12:59:35 +0200

    La til requirements + gitignore

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,19 @@
 from flask import Flask, send_from_directory
 import json
-
+from src.lib import joke
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return send_from_directory("src/pages", "index.html")
+
+
+@app.route('/joke', methods=['GET'])
+def jokes():
+    while True:
+        res = joke.getJoke()
+        return res
 
 if __name__ == '__main__':
     app.run(port=5500, debug=True)
--- backend/src/lib/joke.py
+++ backend/src/lib/joke.py
@@ -8,8 +8,9 @@ def getJoke():
     joke = json.loads(x.text)
     if joke.get('joke') is not None:
         print("joke", joke.get('joke'))
-        return json.dumps({"Joke": joke.get('joke')})
+        return json.dumps({"Setup": None, "Delivery": joke.get('joke')})
     print(f"{joke.get('setup')}: ", f"\n{joke.get('delivery')}", "\n",)
     return json.dumps({"Setup": joke.get('setup'), "Punchline": joke.get('delivery')})
+
 if __name__ == '__main__':
     print(getJoke())
