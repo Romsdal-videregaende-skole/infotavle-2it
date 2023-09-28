39ca1aea118ce76caa7db400e54457da7c5de8c6
bazic <lashav19@o365.mrvgs.no>
2023-09-28 14:34:58 +0200

    Serving av frontend med css

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,20 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, url_for, render_template
 from flask_cors import CORS
 import json
 from src.lib import joke
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="../frontend")
 CORS(app)
 
 
 @app.route('/')
 def index():
-    return send_from_directory("../frontend", "index.html")
+    return render_template("index.html")
 
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory("../frontend", filename)
 
 @app.route('/joke', methods=['GET'])
 def jokes():
