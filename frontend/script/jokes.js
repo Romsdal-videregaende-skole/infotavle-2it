1f6106ec28a5a06c109f3e6893819fe774a2fe1b
Dalimil Paur <dalpau18@o365.mrvgs.no>
2023-09-28 13:58:37 +0200

    jobbet med jokes

--- frontend/script/jokes.js
+++ frontend/script/jokes.js
@@ -1,11 +1,16 @@
 async function getJoke(){
-    const api = new URL('http://127.0.0.1:5500/joke')
+    const api = new URL('http://127.0.0.1:5000/joke')
 
     const response = await fetch(api)
 
     const data = await response.json()
 
-    console.log(data.Setup, data.Delivery)
+    if (data.Setup !== "Null"){
+        console.log(data.Setup, data.Punchline)
+    } else{
+        console.log(data.Punchline)
+    }
+
 
 }
 
