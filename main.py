789c6f0f5780ae79cdcb6d3a62d9036126c38697
bazic <lashav19@o365.mrvgs.no>
2023-09-28 10:20:29 +0200

    init project

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+import json
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return json.dumps({"Index": "main"})
+
+if __name__ == '__main__':
+    app.run(5500, debug=True)
