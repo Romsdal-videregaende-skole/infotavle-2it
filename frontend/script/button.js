46050e3643680a8ed48b4940dcd49d731e238832
Vegard <vegoye27@o365.mrvgs.no>
2023-09-28 14:39:00 +0200

    gjort det litt enklere med baren og nå trenger vi APIen

--- frontend/script/button.js
+++ frontend/script/button.js
@@ -1,14 +1,17 @@
 var i = 0;
+var width = 1; // Initial width
+
 function move() {
   if (i == 0) {
     i = 1;
     var elem = document.getElementById("loading-bar");
-    var width = 1;
     var id = setInterval(frame, 10);
+
     function frame() {
       if (width >= 100) {
         clearInterval(id);
         i = 0;
+        
       } else {
         width++;
         elem.style.width = width + "%";
