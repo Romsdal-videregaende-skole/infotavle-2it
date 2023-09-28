116822fb833803fc9c4aad9cbdf6d8bec46194db
B4Z1C <lashav19@o365.mrvgs.no>
2023-09-29 00:33:24 +0200

    Kommentarer
    Kommentert kode i main

--- backend/main.py
+++ backend/main.py
@@ -6,28 +6,28 @@ from src.lib import joke
 from src.lib.visma import getVisma
 import json
 
-app = Flask(__name__, template_folder="../frontend")
-CORS(app)
+app = Flask(__name__, template_folder="../frontend")  # Oppretter flask app
+CORS(app)  # Flask CORS Står for Cross-Origin Resource Sharing som gjør at vi kan dele info mellom backend og frontend
 
 
-@app.route('/')
+@app.route('/')  # Index path
 def index():
     return render_template("index.html")
 
 
+# /<path:filename> gjør at HTML kan få tak i de statiske elementene
 @app.route('/<path:filename>')
 def serve_static(filename):
     return send_from_directory("../frontend", filename)
 
 
-@app.route('/joke', methods=['GET'])
+@app.route('/joke', methods=['GET'])  # En get request for joke API'en
 def jokes():
-
     res = joke.getJoke()
     return res
 
 
-@app.route('/visma', methods=['GET'])
+@app.route('/visma', methods=['GET'])  # Get request for Visma API'en
 def visma():
     def fetchAPI():
         timeplan = getVisma()
@@ -42,4 +42,5 @@ def visma():
 
 
 if __name__ == '__main__':
+    # starter app i debug mode som gjør at den reloader serveren on save
     app.run(port=5000, debug=True, host="0.0.0.0")
