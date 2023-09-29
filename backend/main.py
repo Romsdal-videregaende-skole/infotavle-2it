3a732349de5266705927092d2c3cb35e6776efab
bazic <lashav19@o365.mrvgs.no>
2023-09-29 13:09:10 +0200

    Returnerer nermeste friminutt

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from flask import Flask, send_from_directory
 from flask_cors import CORS
 import json
 from src.lib import joke
+from src.lib import friminutt
 
 app = Flask(__name__)
 CORS(app)
@@ -12,6 +13,13 @@ def index():
     return send_from_directory("src/pages", "index.html")
 
 
+@app.route('/friminutt')
+def getFriminutt():
+    friminutt_start = ["9:5", "9:55", "10:55", "11:45", "13:00", "13:50", "14:40"]
+    friminutter = friminutt.liste(friminutt_start)
+    print(friminutter)
+    return {"friminutt": friminutter[1]/100}
+
 @app.route('/joke', methods=['GET'])
 def jokes():
 
@@ -19,4 +27,4 @@ def jokes():
     return res
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(port=5000, debug=True, host="0.0.0.0")
