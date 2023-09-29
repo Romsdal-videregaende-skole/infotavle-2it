668b72eeb409710d76c2c5adc983315d0a55f372
bazic <lashav19@o365.mrvgs.no>
2023-09-29 14:16:51 +0200

    Fikset logikk med friminutt

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, send_from_directory
 from flask_cors import CORS
+from datetime import time as Time
+from datetime import datetime, time
 import json
 from src.lib import joke
-from src.lib import friminutt
 
 app = Flask(__name__)
 CORS(app)
@@ -13,12 +14,33 @@ def index():
     return send_from_directory("src/pages", "index.html")
 
 
-@app.route('/friminutt')
+@app.route('/friminutt', methods=['GET'])
 def getFriminutt():
+    
+    def liste(friminutt):
+        today = datetime.now()
+        current_time = datetime.now().time()
+
+        for x in range(len(friminutt)):
+            split_tider = friminutt[x].split(":")
+            res = [eval(i) for i in split_tider]
+            test = datetime(today.year, today.month, today.day, res[0], res[1])
+            if today < test:
+                current_datetime = datetime.combine(datetime.today(), current_time)
+                time_difference = test - current_datetime
+                total_minutes_until_recess = time_difference.total_seconds() / 60
+                percentage_difference = (total_minutes_until_recess / 40) * 100
+
+                # Invert the percentage
+                inverted_percentage = 100 - percentage_difference
+
+                return friminutt[x], inverted_percentage
+            
+
+    # Calculate the break times within the route handler
     friminutt_start = ["9:5", "9:55", "10:55", "11:45", "13:00", "13:50", "14:40"]
-    friminutter = friminutt.liste(friminutt_start)
-    print(friminutter)
-    return {"friminutt": friminutter[1]/100}
+    friminutter = liste(friminutt_start)
+    return {"friminutt": friminutter[1]}
 
 @app.route('/joke', methods=['GET'])
 def jokes():
