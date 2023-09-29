e194ef6f1a3637523ecb7c93d1c18d57e5c00314
Nikita <nikitamozaiko02@gmail.com>
2023-09-29 13:27:07 +0200

    api temperatur FERDIG

--- backend/src/lib/temperatur.py
+++ backend/src/lib/temperatur.py
@@ -5,19 +5,22 @@ refresh_token = '65155c4b9e4d906b7e03d149|7dbe56aeb9e9209c8018f38192ac5d7f'
 
 api_endpoint = 'https://api.netatmo.com/api/getstationsdata'
 
+def getTemp():
+    headers = {
+        'Authorization': f'Bearer {access_token}'
+    }
 
-headers = {
-    'Authorization': f'Bearer {access_token}'
-}
+    response = requests.get(api_endpoint, headers=headers)
 
-response = requests.get(api_endpoint, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
 
-if response.status_code == 200:
-    data = response.json()
+        print(data)
+    else:
+        print(f'Error: {response.status_code} - {response.text}')
+        data = response.text
+        print(data.get("body"))
+    return
 
-    print(data)
-else:
-    print(f'Error: {response.status_code} - {response.text}')
-    data = response.text
-    print(data.get("body"))
-
+if __name__ == '__main__':
+    getTemp()
