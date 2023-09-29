457b15fe4ea883dd976b14681b60c16a1433151b
Dalimil Paur <dalpau18@o365.mrvgs.no>
2023-09-29 14:24:41 +0200

    Vi fikset jokes, og Magnus gjorde at de kom etter intervaller

--- frontend/script/jokes.js
+++ frontend/script/jokes.js
@@ -1,16 +1,22 @@
-async function getJoke(){
+setInterval(async function getJoke(){
+    
     const api = new URL('http://127.0.0.1:5000/joke')
 
     const response = await fetch(api)
 
     const data = await response.json()
 
-    if (data.Setup !== "Null"){
+    if (data.Setup !== ""){
         console.log(data.Setup, data.Punchline)
     } else{
         console.log(data.Punchline)
     }
 
+    var jSetup= JSON.stringify(data.Setup)       
+    var jPunchline = JSON.stringify(data.Punchline)
 
-}
+    //jstring = jstring.replaceALL("[{}]", "")
 
+    document.getElementById("setup").innerHTML = jSetup;
+    document.getElementById("punchline").innerHTML = jPunchline
+},15000)
