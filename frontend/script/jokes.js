cd0a54d8e82fe901a6354d70f3c0e1bf78bdc105
Dalimil Paur <dalpau18@o365.mrvgs.no>
2023-10-05 09:09:57 +0200

    pusher endringer med fikset jokes

--- frontend/script/jokes.js
+++ frontend/script/jokes.js
@@ -1,6 +1,6 @@
 setInterval(async function getJoke(){
     
-    const api = new URL('http://127.0.0.1:5000/joke')
+    const api = new URL('http://127.0.0.1:5000/api/joke')
 
     const response = await fetch(api)
 
@@ -14,8 +14,6 @@ setInterval(async function getJoke(){
 
     var jSetup= JSON.stringify(data.Setup)       
     var jPunchline = JSON.stringify(data.Punchline)
-
-    //jstring = jstring.replaceALL("[{}]", "")
 
     document.getElementById("setup").innerHTML = jSetup;
     document.getElementById("punchline").innerHTML = jPunchline
