cc1f22761da3289c31ae30a4908637191aee1c2e
bazic <lashav19@o365.mrvgs.no>
2023-11-02 10:17:51 +0100

    fikset import error

--- backend/src/lib/visma.py
+++ backend/src/lib/visma.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-import src.lib.kryptering as kryptering
+from src.lib.kryptering import encrypter
 from functools import cache
 from bs4 import BeautifulSoup
 import time
